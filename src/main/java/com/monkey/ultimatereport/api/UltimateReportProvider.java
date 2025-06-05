644921edf64d9e09d1240efb89d5ee4fb4630e9d
MonkeyMoon104 <lollolilo32@gmail.com>
2025-06-05 23:03:12 +0200

    Updated API

--- src/main/java/com/monkey/ultimatereport/api/UltimateReportProvider.java
+++ src/main/java/com/monkey/ultimatereport/api/UltimateReportProvider.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface UltimateReportProvider {
 
-    int createReport(String reporter, String target, String reason);
+    int createReport(String reporter, String target, String reason, boolean bypasscooldown, boolean silent);
 
     int getValidReports(String playerName);
 
